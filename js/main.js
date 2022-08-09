8de557642f12a3c7ad747d471d7a9c0f8fef89d7
Stoupy51 <stoupy51@gmail.com>
2022-08-09 18:57:39 +0200

    Fin de la première version ?

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 
+function background() {
+	document.getElementById("background").style.height = document.getElementsByTagName("body")[0].clientHeight + "px";
+}
+
+window.addEventListener("load", background);
+window.addEventListener("resize", background);
 
